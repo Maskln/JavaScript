d5a935d9c4385fb301420a4ea533961adb90e9cf
Maskln <lyubomir.nanev@gmail.com>
2015-06-03 20:13:10 +0300

    Final version of Functions

--- 06.Functions/Problem7.FirstLargerThanNeighbours.js
+++ 06.Functions/Problem7.FirstLargerThanNeighbours.js
@@ -1,35 +1,35 @@
 //Problem 7. First larger than neighbours
-//Write a function that returns the index of the first element in array that is larger than its neighbours or -1, if there’s no such element.
+//Write a function that returns the index of the first element in array that is larger than its neighbours or -1, if thereï¿½s no such element.
 //    Use the function from the previous exercise.
 
-var numbers = [3, 2, 3, 1, 5, 6, 7, 87, 98, 2, 12],
-    position = 2;
+var numbers = [1, 1, 1, 1, 5, 6, 7, 8, 8, 2, 12];
 
-isBiggerThanNeighbours(position, numbers);
+isBiggerThanNeighbours(numbers);
 
+function isBiggerThanNeighbours(numbers){
+    numbers = numbers || [];
 
-function isBiggerThanNeighbours(position, numbers){
-    var len = numbers.length;
+    var len = numbers.length,
+        position;
 
+    for (position = 0; position < len; position += 1) {
 
-    if(!(position)){
-        if(numbers[position] > numbers[position + 1]){
-            return console.log(position);
-        }else{
-            return -1;
-        }
-    }else if (position === (len - 1)){
-        if (numbers[position] > numbers[position - 1]){
-            return console.log(position);
-        }else{
-            return -1;
-        }
-    }else{
-        if((numbers[position] > numbers[position - 1]) && (numbers[position] > numbers[position + 1])){
-            return console.log(position);
-        }else{
-            return -1;
+        if (!(position)) {
+            if (numbers[position] > numbers[position + 1]) {
+                return console.log('The position of element is: ' + position);
+            }
+        } else if (position === (len - 1)) {
+            if (numbers[position] > numbers[position - 1]) {
+                return console.log('The position of element is: ' + position);
+            } else {
+                return console.log(-1);
+            }
+        } else {
+            if ((numbers[position] > numbers[position - 1]) && (numbers[position] > numbers[position + 1])) {
+                return console.log('The position of element is: ' + position);
+            }
         }
     }
-    return console.log('The value ' + numbers[position] + " is Bigger that its two neighbours? " + isBigger);
 }
+
+
